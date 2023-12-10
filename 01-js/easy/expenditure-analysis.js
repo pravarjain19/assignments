3f977a15bb4f3e96e95846b7fa4f7ff715f6f20f
pravarjain881 <pravarjain881@gmail.com>
2023-12-10 14:51:48 +0530

    expenditure solution

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -13,8 +13,56 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+const { log } = require("console");
+
+class Categories {
+  constructor(category , totalSpent){
+    this.totalSpent = totalSpent
+    this.category = category
+  }
+   getTotalSpent(){
+      return this.totalSpent;
+  }
 }
+
+function calculateTotalSpentByCategory(transactions) {
+
+  let arr = [...transactions];
+  let cat = arr.map((e , i)=>{
+    return e['category']
+    
+  })
+
+  let unique  = Array.from(new Set(cat));
+  let res = [];
+  let total = 0;
+  for(let i = 0 ; i<unique.length ; i++){
+    let ab = {  category : "" ,totalSpent : 0}
+    arr.forEach((e ,j)=>{
+      if(unique[i] == e['category']){
+        total += e['price'];
+      }
+      ab.category = unique[i]
+      ab.totalSpent=total
+    })
+    res[i] = ab;
+    total=0
+  }
+
+    console.log(res);
+  
+  return res;
+}
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 60,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
+];
+calculateTotalSpentByCategory(transactions);
+
 
 module.exports = calculateTotalSpentByCategory;
