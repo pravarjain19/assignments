0a97f79fbc038586d3b616dc6071e93daa8d8aa4
pravarjain881 <pravarjain881@gmail.com>
2023-12-22 22:25:34 +0530

    timecode

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,13 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let before = new Date();
+    let sum = 0 ;
+    for(let i = 0  ; i<n ; i++){
+            sum++;
+    }
+    let after = new Date();
+    return after-before;
 }
+
+console.log(calculateTime(10000000));
